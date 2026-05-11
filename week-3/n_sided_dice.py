28acf1779bdabd0b5e7ec2598859c55e3f5d00fd
Hareshku <hareesh7737@gmail.com>
2026-05-11 19:59:11 +0500

    print 10 random numbers

---
+++ week-3/n_sided_dice.py
@@ -0,0 +1,9 @@
+def main():
+   sides= int(input("How many sides does your dice have? "))
+   result = random.randint(1, sides)
+   print(f"Your roll is {result}")
+
+
+
+if __name__ == '__main__':
+    main()
